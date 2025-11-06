56380ff859131a7a3cb5f2a9999687c6a2a2801a
justtpriya-12 <priyachauhan51574@gmail.com>
2025-11-06 11:14:13 +0530

    practice

---
+++ JAVASCRIPT/DOM/script.js
@@ -0,0 +1,14 @@
+//console.log("Hello world!");
+// const mainHeading = document.getElementById("title");
+
+// const triggerButton = document.getElementById("btn");
+// triggerButton.onclick = () => {
+//     mainHeading.textContent = "Hello from Javascript";
+// }
+
+// const items = document.getElementById("Titleid");
+// const items = document.getElementById("Titleid");
+//const heading = document.querySelector("h1");
+//const items = document.getElementById("Titleid");
+//console.log(items);
+//console.log(heading)
