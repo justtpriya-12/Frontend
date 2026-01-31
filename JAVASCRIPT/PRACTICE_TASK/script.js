fb3324e76b0f4a5d13e701de8562b906292526ec
justtpriya-12 <priyachauhan51574@gmail.com>
2026-01-31 17:16:02 +0530

    change

--- JAVASCRIPT/PRACTICE_TASK/script.js
+++
@@ -1,7 +0,0 @@
-const chess = document.querySelector(".chessboard");
-
-const oddRow =`<div class="square"></div>`.repeat(8);
-const evenRow =`<div class="square-rev"></div>`.repeat(8);
-
-const completeHtml = `${oddRow} ${evenRow}`.repeat(4);
-chess.innerHTML = completeHtml;
